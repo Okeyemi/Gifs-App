cb6dbd17e46e0bdf57fb8b9056e2099f2fec69b6
okeyemi <okeyemitunde@gmail.com>
2024-02-23 06:57:00 +0100

    first commit

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ export const NavBar = ({ onSearch }) => {
   return (
     <div  >
       <div className="md:flex md:gap-72  items-center text-center ">
-      <h1 className="logo text-white bg-[#1a1a1a] p-3 rounded-lg ">GIPGHY APP</h1>
+      <h1 className="logo text-white bg-[#1a1a1a] p-3 rounded-lg ">RERE GIFS APP</h1>
         {/* search? */}
       <div className="mt-5 md:mt-0">
         <Search onSearch={onSearch} />
